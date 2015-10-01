fe302fe1af68432ae63907f2d9ed9e40ebcb5a48
Alice Jenkinson <ed.ch1zuru@gmail.com>
2015-10-01 13:50:26 +1300

    Fix the majority of the VFS bugs

--- hypatia/vfs.py
+++ hypatia/vfs.py
@@ -14,6 +14,10 @@ class IsADirectory(VFSException):
     def __init__(self, path):
         self.path = path
 
+class NotADirectory(VFSException):
+    def __init__(self, path):
+        self.path = path
+
 class FileNotFound(VFSException):
     def __init__(self, path):
         self.path = path
@@ -26,6 +30,13 @@ class VFS(object):
         self._mountpoints = {}
 
     def mount(self, mountpoint, provider):
+        """Mount the given provider at the specified mountpoint.
+
+        Args:
+          mountpoint (str): Mountpoint to use
+          provider (hypatia.vfs.VFSProvider): Provider to mount
+        """
+
         if mountpoint in self._mountpoints:
             raise MountPointInUse(mountpoint)
 
@@ -33,109 +44,191 @@ class VFS(object):
         self._mountpoints[mountpoint]._cache_files()
 
     def unmount(self, mountpoint):
+        """Unmount a provider from the given mountpoint.
+
+        Args:
+          mountpoint (str): Mountpoint to unmount
+        """
+
         if not mountpoint in self._mountpoints:
             return
 
         del self._mountpoints[mountpoint]
 
+    def split(self, path):
+        return self.normalize(path).split(self.PATH_SEPARATOR)
+
+    def normalize(self, path):
+        # Remove root.
+        if path.startswith(self.PATH_SEPARATOR):
+            path = path[len(self.PATH_SEPARATOR):]
+
+        # Split path into directory pieces and remove empty or redundant directories.
+        pieces = [ piece for piece in path.split(self.PATH_SEPARATOR)
+                   if piece and piece != '.' ]
+
+        # Remove parent directory entries.
+        while '..' in pieces:
+            i = pieces.index('..')
+            del pieces[i]
+            # The preceding directory too, of course.
+            if i > 0:
+                del pieces[i - 1]
+
+        return self.PATH_SEPARATOR + self.PATH_SEPARATOR.join(pieces)
+
     def isdir(self, path):
-        spath = path.split(VFS.PATH_SEPARATOR)
-        spath.remove('')
+        """Returns whether or not the given path is a directory.
+
+        Args:
+          path (str): Path to check
+        """
+
+        spath = self.split(path)
 
         # work forwards and try and find a mountpoint matching the file's path
         looked = []
         for i in spath:
-            lookpath = VFS.PATH_SEPARATOR + VFS.PATH_SEPARATOR.join(looked)
+            looked.append(i)
+            lookpath = VFS.PATH_SEPARATOR.join(looked)
 
             if lookpath in self._mountpoints:
-                p = VFS.PATH_SEPARATOR.join(spath[len(looked):])
-                return self._mountpoints[lookpath].isdir(p)
-
-            looked.append(i)
+                return self._mountpoints[lookpath].isdir(spath[len(looked):])
 
         return False
 
-    def exists(self, path):
-        spath = path.split(VFS.PATH_SEPARATOR)
-        spath.remove('')
+    def isfile(self, path):
+        """Returns whether or not the given path is a file.
+
+        Args:
+          path (str): Path to check
+        """
+
+        spath = self.split(path)
 
         # work forwards and try and find a mountpoint matching the file's path
         looked = []
         for i in spath:
-            lookpath = VFS.PATH_SEPARATOR + VFS.PATH_SEPARATOR.join(looked)
+            looked.append(i)
+            lookpath = VFS.PATH_SEPARATOR.join(looked)
 
             if lookpath in self._mountpoints:
-                p = VFS.PATH_SEPARATOR.join(spath[len(looked):])
-                return self._mountpoints[lookpath].exists(p)
+                return not self._mountpoints[lookpath].isdir(spath[len(looked):])
 
+        return False
+
+    def list(self, path):
+        """Returns a list of the files and subdirectories in the given path,
+        including whether the given path is a file or directory.
+        """
+
+        spath = self.split(path)
+        looked = []
+        for i in spath:
             looked.append(i)
+            lookpath = VFS.PATH_SEPARATOR.join(looked)
+
+            if lookpath in self._mountpoints:
+                return self._mountpoints[lookpath].list(spath[len(looked):])
+
+        return []
+
+    def exists(self, path):
+        """Returns whether or not a file exists at the given path.
+
+        Args:
+          path (str): Path to check
+        """
+
+        spath = self.split(path)
+
+        # work forwards and try and find a mountpoint matching the file's path
+        looked = []
+        for i in spath:
+            looked.append(i)
+            lookpath = VFS.PATH_SEPARATOR.join(looked)
+
+            if lookpath in self._mountpoints:
+                return self._mountpoints[lookpath].exists(spath[len(looked):])
 
         return False
 
     def open(self, path):
-        spath = path.split(VFS.PATH_SEPARATOR)
-        spath.remove('')
+        """Open a file with the given path. This returns a (read-only) BytesIO
+        object.
+
+        Args:
+          path (str): Path to open
+        """
+
+        # TODO: Support read/write access, file modes. Implement this with a
+        # File object that controls read/writing to the provider. See the
+        # rave project's File class for details on how this should be 
+        # implemented.
+
+        spath = self.split(path)
 
         # work forwards and try and find a mountpoint matching the file's path
         looked = []
         for i in spath:
-            lookpath = VFS.PATH_SEPARATOR + VFS.PATH_SEPARATOR.join(looked)
+            looked.append(i)
+            lookpath = VFS.PATH_SEPARATOR.join(looked)
 
             if lookpath in self._mountpoints:
-                p = VFS.PATH_SEPARATOR.join(spath[len(looked):])
-                return self._mountpoints[lookpath].open(p)
-
-            looked.append(i)
+                return self._mountpoints[lookpath].open(spath[len(looked):])
 
         raise FileNotFound(path)
 
-# _contents = {
-#     "dirname": {
-#         "isdir": True,
-#         "children": {
-#             "test": {
-#                 "isdir": False,
-#                 "contents": BytesIO(b"test"),
-#             }
-#         },
-#     },
-# }
-
 class VFSProvider(object):
     def __init__(self):
-        self._contents = {}
+        # _contents = {
+        #     "dirname": {
+        #         "isdir": True,
+        #         "children": {
+        #             "test": {
+        #                 "isdir": False,
+        #                 "contents": BytesIO(b"test"),
+        #             }
+        #         },
+        #     },
+        # }
+        self._contents = {"isdir": True, "children": {}}
 
-    def isdir(self, path):
-        if path.startswith("/"):
-            path = path.strip("/")
-
-        spath = path.split(VFS.PATH_SEPARATOR)
+    def isdir(self, spath):
         dir = self._contents
-        if spath[0] in dir:
-            dir = dir[spath[0]]
-        else:
-            raise FileNotFound(path)
 
-        for fragment in spath[1:]:
+        for fragment in spath:
             if fragment not in dir["children"]:
-                raise FileNotFound(path)
+                raise FileNotFound(VFS.PATH_SEPARATOR.join(spath))
 
             dir = dir["children"][fragment]
 
         return dir["isdir"]
 
-    def exists(self, path):
-        if path.startswith("/"):
-            path = path.strip("/")
-
-        spath = path.split(VFS.PATH_SEPARATOR)
+    def list(self, spath):
         dir = self._contents
-        if spath[0] in dir:
-            dir = dir[spath[0]]
-        else:
-            return False
+        for fragment in spath:
+            if fragment not in dir["children"]:
+                raise FileNotFound(VFS.PATH_SEPARATOR.join(spath))
 
-        for fragment in spath[1:]:
+            dir = dir["children"][fragment]
+
+        if not dir["isdir"]:
+            raise NotADirectory(VFS.PATH_SEPARATOR.join(spath))
+
+        out = []
+        for i in dir["children"].keys():
+            out.append({
+                "name": i,
+                "isdir": dir["children"][i]["isdir"],
+            })
+
+        return out
+
+    def exists(self, spath):
+        dir = self._contents
+
+        for fragment in spath:
             if fragment not in dir["children"]:
                 return False
 
@@ -143,20 +236,11 @@ class VFSProvider(object):
 
         return True
 
-    def open(self, path):
-        if path.startswith("/"):
-            path = path.strip("/")
-
-        spath = path.split(VFS.PATH_SEPARATOR)
+    def open(self, spath):
         dir = self._contents
-        if spath[0] in dir:
-            dir = dir[spath[0]]
-        else:
-            raise FileNotFound(path)
-
-        for fragment in spath[1:]:
+        for fragment in spath:
             if fragment not in dir["children"]:
-                raise FileNotFound(path)
+                raise FileNotFound(VFS.PATH_SEPARATOR.join(spath))
 
             dir = dir["children"][fragment]
 
@@ -177,26 +261,8 @@ class FilesystemProvider(VFSProvider):
         for root, dirs, files in os.walk(self._path):
             sroot = root[len(self._path):].split(os.sep)[1:]
 
-            if len(sroot) is 0:
-                for file in files:
-                    with open(os.path.join(self._path, file), 'r') as fh:
-                        contents = fh.read()
-
-                    self._contents[file] = {
-                        "isdir": False,
-                        "contents": io.BytesIO(contents),
-                    }
-
-                continue
-    
-            if not sroot[0] in self._contents:
-                self._contents[sroot[0]] = {
-                    "isdir": True,
-                    "children": {},
-                }
-
-            nest = self._contents[sroot[0]]
-            for fragment in sroot[1:]:
+            nest = self._contents
+            for fragment in sroot:
                 if not fragment in nest["children"]:
                     nest["children"][fragment] = {
                         "isdir": True,
